e5e47141a5473f1dc73a484c460b3ef0617b1014
dinesh nagpal <133826705+sabby74@users.noreply.github.com>
2023-11-05 23:36:10 -0500

    fields update

--- toplan/serializers.py
+++ toplan/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from .models import Toplan
 
 
-class ToplanSerializer(serializers.ModelSerializer):
+class ToplanSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Toplan
-        fields = '__all__'
+        fields = ['id','subject', 'details', 'date', 'time', 'is_completed', 'created_at', 'updated_at']
