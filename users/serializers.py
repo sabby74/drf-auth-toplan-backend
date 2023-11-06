d76188364a5c18e4d3d818220f290865e492a7c0
dinesh nagpal <133826705+sabby74@users.noreply.github.com>
2023-11-05 21:35:36 -0500

    added code

--- users/serializers.py
+++ users/serializers.py
@@ -2,7 +2,7 @@ from djoser.serializers import UserCreateSerializer, UserSerializer
 from rest_framework import serializers
 from . import models
 
-class UserCreateSerializer(UserCreateSerializer):
+class UsersCreateSerializer(UserCreateSerializer):
     class Meta(UserCreateSerializer.Meta):
         model = models.User
         fields = ('id', 'email', 'username', 'password', 'avatar')
