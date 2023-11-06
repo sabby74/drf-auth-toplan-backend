882848b48184e05fc96e29b91a65846747f75498
dinesh nagpal <133826705+sabby74@users.noreply.github.com>
2023-11-06 00:19:21 -0500

    permission class added to views.py

--- toplan/views.py
+++ toplan/views.py
@@ -1,5 +1,6 @@
 from rest_framework import generics
 from .models import Toplan
+from rest_framework import permissions
 from .serializers import ToplanSerializer
 
 # Create your views here.
@@ -7,6 +8,7 @@ from .serializers import ToplanSerializer
 class ToplanList(generics.ListCreateAPIView):
     queryset = Toplan.objects.all()
     serializer_class = ToplanSerializer
+    permission_classes = [permissions.AllowAny] #Coule be [permissions.IsAuthenticated]
 
 
 class ToplanDetail(generics.RetrieveUpdateDestroyAPIView):
