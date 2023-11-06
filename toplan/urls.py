2cc0445cc20489be86b89d9472e801b99831eb5e
dinesh nagpal <133826705+sabby74@users.noreply.github.com>
2023-11-06 01:03:02 -0500

    urls update

--- toplan/urls.py
+++ toplan/urls.py
@@ -1,11 +1,11 @@
-# from django.urls import path
-# from .views import ToplanList, ToplanDetail
-# from users.views import UsersList
+from django.urls import path
+from .views import ToplanList, ToplanDetail
+from users.views import UsersList
 
 
-# urlpatterns = [
-#     path('toplan/', ToplanList.as_view(), name='toplan_list'),
-#     path('toplan/<int:pk>/', ToplanDetail.as_view(), name='toplan_detail'),
-#     path('users/', UsersList.as_view(), name='user_list'),
+urlpatterns = [
+    path('toplan/', ToplanList.as_view(), name='toplan_list'),
+    path('toplan/<int:pk>/', ToplanDetail.as_view(), name='toplan_detail'),
+    path('users/', UsersList.as_view(), name='user_list'),
 
-# ]
+]
