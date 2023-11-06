7bbc718372e285f15a544e52b5f19821b3fd451b
dinesh nagpal <133826705+sabby74@users.noreply.github.com>
2023-11-05 19:03:54 -0500

    user moden and superuser created

---
+++ toplan/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ToplanConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'toplan'
--- toplanproject/settings.py
+++ toplanproject/settings.py
@@ -50,6 +50,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'corsheaders', # CORS
     'djoser', # DJOSER
+    'rest_framework', # REST FRAMEWORK
+    'rest_framework.authtoken', # REST FRAMEWORK
 
     'users', # USERS
 ]
@@ -65,6 +67,30 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        # What we'll use for our API
+        'rest_framework.authentication.TokenAuthentication',
+        # What we'll use for the browseable API
+        'rest_framework.authentication.SessionAuthentication',
+        # Basic Authentication should be removed in production
+        'rest_framework.authentication.BasicAuthentication',
+    ],
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
+
+DJOSER = {
+    'LOGIN_FIELD': 'email',
+    'USER_CREATE_PASSWORD_RETYPE': True,
+    'SERIALIZERS': {
+        'user_create': 'users.serializers.UserCreateSerializer',
+        'user': 'users.serializers.UserCreateSerializer'
+    }
+}
 
 ROOT_URLCONF = 'toplanproject.urls'
 
@@ -137,3 +163,5 @@ CORS_ALLOW_ALL_ORIGINS = True
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL='users.User'
